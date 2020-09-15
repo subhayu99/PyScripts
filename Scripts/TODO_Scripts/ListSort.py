b2488d5a1dbcf9ff301372e37096c2f617ba0203
Deepraj Rakshit <deep.papai07@gmail.com>
2020-09-15 15:36:51 +0530

    Completed TODO task

--- Scripts/TODO_Scripts/ListSort.py
+++ Scripts/TODO_Scripts/ListSort.py
@@ -1,4 +1,17 @@
 arr = [12, 820, 73, 23, 41, 0, -1, 0000.001, 1100.00, 101.001, 20, 560]
 
-# TODO: Make a function to Sort the above list "arr" and print it to on the screen.
+def selection_sort(array):
+    for i in range(0,len(array)):
+        minval=i                            # Assuming the minimum value is the first element 
+        for j in range(i+1, len(array)):    # Updating the minimum value
+            if array[minval] > array[j]: 
+                minval = j
+
+        array[i], array[minval] = array[minval], array[i] #swapping values to sort
+
+    print (array)
+
+selection_sort(arr)
+
+
 
