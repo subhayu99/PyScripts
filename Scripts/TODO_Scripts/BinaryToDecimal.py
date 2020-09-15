e2d669fe136c8ca35f24f5a9db24fbe39c8caab2
Deepraj Rakshit <deep.papai07@gmail.com>
2020-09-16 00:20:02 +0530

    Completed TODO task

--- Scripts/TODO_Scripts/BinaryToDecimal.py
+++ Scripts/TODO_Scripts/BinaryToDecimal.py
@@ -1,1 +1,9 @@
-# TODO: Build a program that accepts a Binary number from the user and print its equivalent Decimal number.
+num = int(input("enter any binary number"))
+sum = 0
+i = 0
+while num!=0:
+    rem = num % 10
+    sum = sum + rem * pow(2,i)
+    num = int(num/10)
+    i = i+1
+print("Decimal number:", sum)
