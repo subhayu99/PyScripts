292d20a2ffe4be426c1f56587d300f27faad391d
Deepraj Rakshit <deep.papai07@gmail.com>
2020-09-17 00:11:40 +0530

    Completed TODO task

--- Scripts/TODO_Scripts/ListComprehension.py
+++ Scripts/TODO_Scripts/ListComprehension.py
@@ -1,9 +1,21 @@
 #This function is an implementation of how list comprehension works
 
-# TODO: Build a program on your own which works like this without using list comprehension.
-
 n = int(input("Enter a number as a range: "))
 print("A list of numbers of squares up to the given number is:")
-print([x**2 for x in range(n)]) 
+
+'''print([x**2 for x in range(n)])'''  #Using List Comprehension
+for i in range(1,n+1):
+    if (i<n):
+        print(i**2,end=', ')
+    else:
+        print(i**2)
+
+print()
 print("Converting the list of numbers till the given number to string data type:")
-print([str(x) for x in range(n)])
+
+'''print([str(x) for x in range(n)])''' #Using List Comprehension
+for i in range(0,n+1):
+    if (i<n):
+        print(str(i),end=', ')
+    else:
+        print(str(i))
