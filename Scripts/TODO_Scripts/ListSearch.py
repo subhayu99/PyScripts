c0fd771837c3eaabab66fa03cac0e67f5d1fafa9
Deepraj Rakshit <deep.papai07@gmail.com>
2020-09-17 00:35:10 +0530

    Completed TODO task

--- Scripts/TODO_Scripts/ListSearch.py
+++ Scripts/TODO_Scripts/ListSearch.py
@@ -1,4 +1,17 @@
 arr = [12, 82, 73, 23, 41, 0, -1, 0000.001, 1100.00, 101.001, 20, 560]
 
-# TODO: Make a function to print the largest number from the above list "arr".
+def Insertion_Sort(arr):                #Using Insertion Sort to sort the array
+    for i in range(1, len(arr)):
+        key = arr[i]
+
+        j = i-1
+        while j >=0 and key < arr[j] : 
+                arr[j+1] = arr[j] 
+                j -= 1
+        arr[j+1] = key
+
+    print("The largest number: ",arr[-1]) # Prining the last element of the array.
+
+
+Insertion_Sort(arr)
 
