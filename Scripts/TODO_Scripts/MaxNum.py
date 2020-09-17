2c8db83267a1f5cbab2f6b145a08c608777dfd3b
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2020-09-17 22:14:05 +0530

    Update MaxNum.py

--- Scripts/TODO_Scripts/MaxNum.py
+++ Scripts/TODO_Scripts/MaxNum.py
@@ -1,3 +1,11 @@
-arr =  (12, 820, 73, 23, 41, 0, -1, 0000.001, 1100.00, 101.001, 20, 560)
+# Program to find the greatest number jn a list
+arr=[1,2,3,4,5]
 
-# TODO: Create a program to print the Greatest number from "arr".
+max=arr[0]
+n=len(arr)
+
+for i in range(i,n):
+    if arr[i]>max: 
+       max=arr[i]
+
+print(max)
