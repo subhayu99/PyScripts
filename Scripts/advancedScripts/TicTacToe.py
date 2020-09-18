147e2009e5a47271f46914eb7d4b1039ee7f72fc
Deepraj Rakshit <deep.papai07@gmail.com>
2020-09-19 00:02:19 +0530

    Added GUI features

--- Scripts/advancedScripts/TicTacToe.py
+++ Scripts/advancedScripts/TicTacToe.py
@@ -1,150 +1,324 @@
-board = [' ' for x in range(10)]
+from tkinter import *
+import tkinter.messagebox
+
+root = Tk()
+root.geometry("1350x750+0+0")
+root.title("Tic Tac Toe")
+root.configure(background='mistyrose')
+Tops = Frame(root, bg='blue', pady=2, width=1350, height=100, relief=RIDGE)
+Tops.grid(row=0, column=0)
+flag = 0
+
+lblTitle = Label(Tops, font=('arial', 50, 'bold'), text=" Tic Tac Toe", bd=21, fg='dodgerblue', bg='mistyrose',
+                 justify=CENTER)
+lblTitle.grid(row=0, column=0)
+
+MainFrame = Frame(root, bg='#ffb84d', pady=2, width=1350, height=100, relief=RIDGE)
+MainFrame.grid(row=1, column=0)
+
+LeftFrame = Frame(MainFrame, bd=10, width=750, height=500, pady=2, padx=10, bg='#ffb84d', relief=RIDGE)
+LeftFrame.pack(side=LEFT)
+
+RightFrame = Frame(MainFrame, bd=10, width=750, height=500, padx=10, pady=2, bg='#ffb84d', relief=RIDGE)
+RightFrame.pack(side=RIGHT)
+
+RightFrame1 = Frame(RightFrame, bd=10, width=600, height=200, padx=10, pady=2, bg='#ffb84d', relief=RIDGE)
+RightFrame1.grid(row=0, column=0)
+
+RightFrame2 = Frame(RightFrame, bd=10, width=560, height=360, padx=10, pady=2, bg='#ffb84d', relief=RIDGE)
+RightFrame2.grid(row=1, column=0)
+
+PlayerX = IntVar()
+PlayerO = IntVar()
+
+PlayerX.set(0)
+PlayerO.set(0)
+Player1 = StringVar()
+Player2 = StringVar()
+pa = StringVar()
+pb = StringVar()
+
+buttons = StringVar()
+click = True
 
 
-# inserting input to the board
-def insertLetter(letter, pos):
-    board[pos] = letter
+def checker(buttons):
+    global click
+    if buttons["text"] == " " and click == True:
+        buttons["text"] = "X"
+        buttons.configure(background="white", fg='blue')
+        click = False
+        scorekeeper()
+    elif buttons["text"] == " " and click == False:
+        buttons["text"] = "O"
+        buttons.configure(background="white", fg='red')
+        click = True
+        scorekeeper()
 
 
-# checks if space is free on board
-def spaceIsFree(pos):
-    return board[pos] == ' '
+def scorekeeper():
+    global pa, pb, Player1, Player2
+    if (button1["text"] == "X" and button2["text"] == "X" and button3["text"] == "X"):
+        button1.configure(background='#90ff0a')
+        button2.configure(background='#90ff0a')
+        button3.configure(background='#90ff0a')
+        n = int(PlayerX.get())
+        score = (n + 1)
+        PlayerX.set(score)
+        pa = "Congratulations " + Player1.get() + " you have Won the Game!!"
+        tkinter.messagebox.showinfo("Tic Tac Toe", pa)
+        reset()
+    if (button4["text"] == "X" and button5["text"] == "X" and button6["text"] == "X"):
+        button4.configure(background='#90ff0a')
+        button5.configure(background='#90ff0a')
+        button6.configure(background='#90ff0a')
+        n = int(PlayerX.get())
+        score = (n + 1)
+        PlayerX.set(score)
+        pa = "Congratulations " + Player1.get() + " you have Won the Game!!"
+        tkinter.messagebox.showinfo("Tic Tac Toe", pa)
+        reset()
+    if (button7["text"] == "X" and button8["text"] == "X" and button9["text"] == "X"):
+        button7.configure(background='#90ff0a')
+        button8.configure(background='#90ff0a')
+        button9.configure(background='#90ff0a')
+        n = int(PlayerX.get())
+        score = (n + 1)
+        PlayerX.set(score)
+        pa = Player1.get() + " Won the Game!!"
+        tkinter.messagebox.showinfo(" Tic Tac Toe", pa)
+        reset()
+    if (button1["text"] == "X" and button4["text"] == "X" and button7["text"] == "X"):
+        button1.configure(background='#90ff0a')
+        button4.configure(background='#90ff0a')
+        button7.configure(background='#90ff0a')
+        n = int(PlayerX.get())
+        score = (n + 1)
+        PlayerX.set(score)
+        pa = Player1.get() + " Won the Game!!"
+        tkinter.messagebox.showinfo(" Tic Tac Toe", pa)
+        reset()
+    if (button5["text"] == "X" and button2["text"] == "X" and button8["text"] == "X"):
+        button5.configure(background='#90ff0a')
+        button2.configure(background='#90ff0a')
+        button8.configure(background='#90ff0a')
+        n = int(PlayerX.get())
+        score = (n + 1)
+        PlayerX.set(score)
+        pa = Player1.get() + " Won the Game!!"
+        tkinter.messagebox.showinfo(" Tic Tac Toe", pa)
+        reset()
+    if (button6["text"] == "X" and button9["text"] == "X" and button3["text"] == "X"):
+        button6.configure(background='#90ff0a')
+        button9.configure(background='#90ff0a')
+        button3.configure(background='#90ff0a')
+        n = int(PlayerX.get())
+        score = (n + 1)
+        PlayerX.set(score)
+        pa = Player1.get() + " Won the Game!!"
+        tkinter.messagebox.showinfo(" Tic Tac Toe", pa)
+        reset()
+    if (button1["text"] == "X" and button5["text"] == "X" and button9["text"] == "X"):
+        button1.configure(background='#90ff0a')
+        button5.configure(background='#90ff0a')
+        button9.configure(background='#90ff0a')
+        n = int(PlayerX.get())
+        score = (n + 1)
+        PlayerX.set(score)
+        pa = Player1.get() + " Won the Game!!"
+        tkinter.messagebox.showinfo(" Tic Tac Toe", pa)
+        reset()
+    if (button5["text"] == "X" and button7["text"] == "X" and button3["text"] == "X"):
+        button5.configure(background='#90ff0a')
+        button7.configure(background='#90ff0a')
+        button3.configure(background='#90ff0a')
+        n = int(PlayerX.get())
+        score = (n + 1)
+        PlayerX.set(score)
+        pa = Player1.get() + " Won the Game!!"
+        tkinter.messagebox.showinfo(" Tic Tac Toe", pa)
+        reset()
+    # for O
+    if (button1["text"] == "O" and button2["text"] == "O" and button3["text"] == "O"):
+        button1.configure(background='#90ff0a')
+        button2.configure(background='#90ff0a')
+        button3.configure(background='#90ff0a')
+        n = int(PlayerO.get())
+        score = (n + 1)
+        PlayerO.set(score)
+        pb = Player2.get() + " Won the Game!!"
+        tkinter.messagebox.showinfo(" Tic Tac Toe", pb)
+        reset()
+    if (button4["text"] == "O" and button5["text"] == "O" and button6["text"] == "O"):
+        button4.configure(background='#90ff0a')
+        button5.configure(background='#90ff0a')
+        button6.configure(background='#90ff0a')
+        n = int(PlayerO.get())
+        score = (n + 1)
+        PlayerO.set(score)
+        pb = Player2.get() + " Won the Game!!"
+        tkinter.messagebox.showinfo(" Tic Tac Toe", pb)
+        reset()
+    if (button7["text"] == "O" and button8["text"] == "O" and button9["text"] == "O"):
+        button7.configure(background='#90ff0a')
+        button8.configure(background='#90ff0a')
+        button9.configure(background='#90ff0a')
+        n = int(PlayerO.get())
+        score = (n + 1)
+        PlayerO.set(score)
+        pb = Player2.get() + " Won the Game!!"
+        tkinter.messagebox.showinfo(" Tic Tac Toe", pb)
+        reset()
+    if (button1["text"] == "O" and button4["text"] == "O" and button7["text"] == "O"):
+        button1.configure(background='#90ff0a')
+        button4.configure(background='#90ff0a')
+        button7.configure(background='#90ff0a')
+        n = int(PlayerO.get())
+        score = (n + 1)
+        PlayerO.set(score)
+        pb = Player2.get() + " Won the Game!!"
+        tkinter.messagebox.showinfo(" Tic Tac Toe", pb)
+        reset()
+    if (button5["text"] == "O" and button2["text"] == "O" and button8["text"] == "O"):
+        button5.configure(background='#90ff0a')
+        button2.configure(background='#90ff0a')
+        button8.configure(background='#90ff0a')
+        n = int(PlayerO.get())
+        score = (n + 1)
+        PlayerO.set(score)
+        pb = Player2.get() + " Won the Game!!"
+        tkinter.messagebox.showinfo(" Tic Tac Toe", pb)
+        reset()
+    if (button6["text"] == "O" and button9["text"] == "O" and button3["text"] == "O"):
+        button9.configure(background='#90ff0a')
+        button6.configure(background='#90ff0a')
+        button3.configure(background='#90ff0a')
+        n = int(PlayerO.get())
+        score = (n + 1)
+        PlayerO.set(score)
+        pb = Player2.get() + " Won the Game!!"
+        tkinter.messagebox.showinfo(" Tic Tac Toe", pb)
+        reset()
+    if (button1["text"] == "O" and button5["text"] == "O" and button9["text"] == "O"):
+        button1.configure(background='#90ff0a')
+        button5.configure(background='#90ff0a')
+        button9.configure(background='#90ff0a')
+        n = int(PlayerO.get())
+        score = (n + 1)
+        PlayerO.set(score)
+        pb = Player2.get() + " Won the Game!!"
+        tkinter.messagebox.showinfo(" Tic Tac Toe", pb)
+        reset()
+    if (button5["text"] == "O" and button7["text"] == "O" and button3["text"] == "O"):
+        button3.configure(background='#90ff0a')
+        button5.configure(background='#90ff0a')
+        button7.configure(background='#90ff0a')
+        n = int(PlayerO.get())
+        score = (n + 1)
+        PlayerO.set(score)
+        pb = Player2.get() + " Won the Game!!"
+        tkinter.messagebox.showinfo(" Tic Tac Toe", pb)
+        reset()
 
 
-# prints the board interface
-def printBoard(board):
-    print('   |   |   ')
-    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
-    print('   |   |   ')
-    print('------------')
-    print('   |   |   ')
-    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
-    print('   |   |   ')
-    print('------------')
-    print('   |   |   ')
-    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
-    print('   |   |   ')
+def reset():
+    button1["text"] = " "
+    button2["text"] = " "
+    button3["text"] = " "
+    button4["text"] = " "
+    button5["text"] = " "
+    button6["text"] = " "
+    button7["text"] = " "
+    button8["text"] = " "
+    button9["text"] = " "
+
+    button1.configure(background='white')
+    button2.configure(background='white')
+    button3.configure(background='white')
+    button4.configure(background='white')
+    button5.configure(background='white')
+    button6.configure(background='white')
+    button7.configure(background='white')
+    button8.configure(background='white')
+    button9.configure(background='white')
 
 
-# checks if board is full
-def isBoardFull(board):
-    if board.count(' ') > 1:
-        return False
-    else:
-        return True
+def Newgame():
+    global pa, pb, Player1, Player2
+    reset()
+    PlayerX.set(0)
+    PlayerO.set(0)
+    Player1 = StringVar()
+    Player2 = StringVar()
+
+    nmPlayerX = Entry(RightFrame1, font=('arial', 40, 'bold'), bd=3, fg="black", textvariable=Player1, width=7,
+                      justify=LEFT).grid(row=0, column=1)
+    nmPlayerO = Entry(RightFrame1, font=('arial', 40, 'bold'), bd=3, fg="black", textvariable=Player2, width=7,
+                      justify=LEFT).grid(row=1, column=1)
 
 
-# checks who's the winner
-def IsWinner(b, l):
-    return ((b[1] == l and b[2] == l and b[3] == l) or
-            (b[4] == l and b[5] == l and b[6] == l) or
-            (b[7] == l and b[8] == l and b[9] == l) or
-            (b[1] == l and b[4] == l and b[7] == l) or
-            (b[2] == l and b[5] == l and b[8] == l) or
-            (b[3] == l and b[6] == l and b[9] == l) or
-            (b[1] == l and b[5] == l and b[9] == l) or
-            (b[3] == l and b[5] == l and b[7] == l))
+lblPlayerX = Label(RightFrame1, font=('arial', 40, 'bold'), text="Player X :", padx=2, pady=2, bg="yellow", fg="black")
+lblPlayerX.grid(row=0, column=0, sticky=W)
 
+nmPlayerX = Entry(RightFrame1, font=('arial', 40, 'bold'), bd=3, fg="black", textvariable=Player1, width=7,
+                  justify=LEFT).grid(row=0, column=1)
 
-# tracks player's move
-def playerMove():
-    run = True
-    while run:
-        move = input("please select a position to enter the X between 1 to 9: ")
-        try:
-            move = int(move)
-            if move > 0 and move < 10:
-                if spaceIsFree(move):
-                    run = False
-                    insertLetter('X', move)
-                else:
-                    print('Sorry, this space is occupied')
-            else:
-                print('please type a number between 1 and 9')
+txtPlayerX = Button(RightFrame1, font=('arial', 26, 'bold'), bd=2, fg="black", bg="yellow", textvariable=PlayerX,
+                    width=4,
+                    justify=LEFT).grid(row=0, column=2)
 
-        except:
-            print('Please type a number')
+lblPlayerO = Label(RightFrame1, font=('arial', 40, 'bold'), text="Player O :", padx=2, pady=2, bg="yellow", fg="black")
+lblPlayerO.grid(row=1, column=0, sticky=W)
 
+nmPlayerO = Entry(RightFrame1, font=('arial', 40, 'bold'), bd=3, fg="black", textvariable=Player2, width=7,
+                  justify=LEFT).grid(row=1, column=1)
 
-# tracks computer's move
-def computerMove():
-    possibleMoves = [x for x, letter in enumerate(board) if letter == ' ' and x != 0]
-    move = 0
+txtPlayerO = Button(RightFrame1, font=('arial', 26, 'bold'), bd=2, fg="black", bg="yellow", textvariable=PlayerO,
+                    width=4,
+                    justify=LEFT).grid(row=1, column=2)
 
-    for let in ['O', 'X']:
-        for i in possibleMoves:
-            boardcopy = board[:]
-            boardcopy[i] = let
-            if IsWinner(boardcopy, let):
-                move = i
-                return move
+btnReset = Button(RightFrame2, text="Reset", font=('arial', 40, 'bold'), height=1, width=15, fg='white', bg='dodgerblue',
+                  command=reset)
+btnReset.grid(row=1, column=0, padx=1, pady=11)
 
-    cornersOpen = []
-    for i in possibleMoves:
-        if i in [1, 3, 7, 9]:
-            cornersOpen.append(i)
+btnNewGame = Button(RightFrame2, text="New  Game", font=('arial', 40, 'bold'), height=1, width=15, fg='white',
+                    bg='dodgerblue', command=Newgame)
+btnNewGame.grid(row=2, column=0, padx=1, pady=11)
 
-    if len(cornersOpen) > 0:
-        move = selectRandom(cornersOpen)
-        return move
+button1 = Button(LeftFrame, text=" ", font=('arial', 30, 'bold'), height=3, width=8, bg='white',
+                 command=lambda: checker(button1))
+button1.grid(row=1, column=0, sticky=S + N + E + W)
 
-    if 5 in possibleMoves:
-        move = 5
-        return move
+button2 = Button(LeftFrame, text=" ", font=('arial', 30, 'bold'), height=3, width=8, bg='white',
+                 command=lambda: checker(button2))
+button2.grid(row=1, column=1, sticky=S + N + E + W)
 
-    edgesOpen = []
-    for i in possibleMoves:
-        if i in [2, 4, 6, 8]:
-            edgesOpen.append(i)
+button3 = Button(LeftFrame, text=" ", font=('arial', 30, 'bold'), height=3, width=8, bg='white',
+                 command=lambda: checker(button3))
+button3.grid(row=1, column=2, sticky=S + N + E + W)
 
-    if len(edgesOpen) > 0:
-        move = selectRandom(edgesOpen)
-        return move
+button4 = Button(LeftFrame, text=" ", font=('arial', 30, 'bold'), height=3, width=8, bg='white',
+                 command=lambda: checker(button4))
+button4.grid(row=2, column=0, sticky=S + N + E + W)
 
+button5 = Button(LeftFrame, text=" ", font=('arial', 30, 'bold'), height=3, width=8, bg='white',
+                 command=lambda: checker(button5))
+button5.grid(row=2, column=1, sticky=S + N + E + W)
 
-# helps computer to choose appropriate move
-def selectRandom(li):
-    import random
-    ln = len(li)
-    r = random.randrange(0, ln)
-    return li[r]
+button6 = Button(LeftFrame, text=" ", font=('arial', 30, 'bold'), height=3, width=8, bg='white',
+                 command=lambda: checker(button6))
+button6.grid(row=2, column=2, sticky=S + N + E + W)
 
+button7 = Button(LeftFrame, text=" ", font=('arial', 30, 'bold'), height=3, width=8, bg='white',
+                 command=lambda: checker(button7))
+button7.grid(row=3, column=0, sticky=S + N + E + W)
 
-# main function of the game
-def main():
-    print("Welcome to the game!")
-    printBoard(board)
+button8 = Button(LeftFrame, text=" ", font=('arial', 30, 'bold'), height=3, width=8, bg='white',
+                 command=lambda: checker(button8))
+button8.grid(row=3, column=1, sticky=S + N + E + W)
 
-    while not (isBoardFull(board)):
-        if not (IsWinner(board, 'O')):
-            playerMove()
-            printBoard(board)
-        else:
-            print("sorry you loose!")
-            break
+button9 = Button(LeftFrame, text=" ", font=('arial', 30, 'bold'), height=3, width=8, bg='white',
+                 command=lambda: checker(button9))
+button9.grid(row=3, column=2, sticky=S + N + E + W)
 
-        if not (IsWinner(board, 'X')):
-            move = computerMove()
-            if move == 0:
-                print(" ")
-            else:
-                insertLetter('O', move)
-                print('computer placed an o on position: ', move)
-                printBoard(board)
-        else:
-            print("you win!")
-            break
-
-    if isBoardFull(board):
-        print("Tie game")
-
-
-# message prompt for the player
-while True:
-    x = input("Do you want to play again? (y/n): ")
-    if x.lower() == 'y':
-        board = [' ' for x in range(10)]
-        print('--------------------')
-        main()
-    else:
-        break
+root.mainloop()
