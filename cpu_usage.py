bd445ecc1a6600c117ff10a297307a011dc9fab1
yash <universeyash4@gmail.com>
2020-09-20 20:33:39 +0530

    added comment at start of script to explain waht script does

--- cpu_usage.py
+++ cpu_usage.py
@@ -1,3 +1,19 @@
+#This script will check cpu usage in percentage
+
+'''when calling the file name for running the script
+    there are there are 3 ways you run this script
+    1st: By calling just the script name after the "Python" this runs the
+         script and time for wait will be 1s and optimal percent 75%
+         Python cpu_usage.py
+    2nd: By calling script name after the "Python" and passing a number this
+        runs the script and time for wait will be number passed and optimal
+         percent 75%
+         Python cpu_usage.py 5
+    3rd: By calling script name after the "Python" and passing a 2 numbers
+         this runs the script and time for wait will be number passed
+         and optimal percent another number passed
+         Python cpu_usage.py 5 75'''
+
 import psutil
 import sys
 
