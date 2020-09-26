d13ee4ec881a284d94f6eefc0d2442367eec6ac7
shalini <sk6048699@gmail.com>
2020-09-27 01:10:43 +0530

    Completed TODO Task

--- Scripts/TODO_Scripts/PyCalculator.py
+++ Scripts/TODO_Scripts/PyCalculator.py
@@ -1,18 +1,18 @@
 # Python program for a simple calculator 
-  
-# Function to add two numbers  
+import math
+# Function to Add two numbers  
 def add(num1, num2): 
     return num1 + num2 
   
-# Function to subtract two numbers  
+# Function to Subtract two numbers  
 def subtract(num1, num2): 
     return num1 - num2 
 
-# Function to multiply two numbers 
+# Function to Multiply two numbers 
 def multiply(num1, num2): 
     return num1 * num2 
   
-# Function to divide two numbers 
+# Function to Divide two numbers 
 def divide(num1, num2): 
     return num1 / num2   
 
@@ -20,10 +20,13 @@ print("Please select operation -\n"\
         "1. Add\n"\
         "2. Subtract\n"\
         "3. Multiply\n"\
-        "4. Divide\n")
+        "4. Divide\n"\
+        "5. sin\n"\
+        "6. cos\n"\
+        "7. tan\n")\
 
 # Take input from the user  
-select = int(input("Select operation among 1, 2, 3, 4: ")) 
+select = int(input("Select operation among 1, 2, 3, 4, 5, 6, 7: ")) 
   
 number_1 = int(input("Enter first number: ")) 
 
@@ -42,5 +45,26 @@ elif select == 3:
 elif select == 4: 
     print("\n", number_1, "/", number_2, "=", divide(number_1, number_2))
 
+elif select == 5:
+    ch=int(input("Choose the number: "))
+    if (ch==1):
+        print("\nSin of ", number_1, "=", math.sin(number_1))
+    else:
+        print("\nSin of ", number_2, "=", math.sin(number_2))
+
+elif select == 6:
+    ch=int(input("Choose the number: "))
+    if (ch==1):
+        print("\nCos of ", number_1, "=", math.cos(number_1))
+    else:
+        print("\nCos of ", number_2, "=", math.cos(number_2))
+
+elif select == 7:
+    ch=int(input("Choose the number: "))
+    if (ch==1):
+        print("\nTan of ", number_1,"=", math.tan(number_1))
+    else:
+        print("\nTan of ", number_2,"=", math.tan(number_2))
+
 else: 
-    print("\nInvalid input") 
+    print("\nInvalid input")
