de3759cf49f7b25703247c0fca6b3a48dfbf66ec
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2020-09-27 15:52:14 +0530

    Update PyCalculator.py

--- Scripts/TODO_Scripts/PyCalculator.py
+++ Scripts/TODO_Scripts/PyCalculator.py
@@ -18,24 +18,24 @@ def divide(num1, num2):
 	
 # Function to find Sin of a number
 def sinnum(num1):
-	return math.sin(num1)
+    return math.sin(num1)
 
 # Function to find Cos of a number
 def cosnum(num1):
-	return math.cos(num1)
+    return math.cos(num1)
 
 # Function to find Tan of a number
 def tannum(num1):
-	return math.tan(num1)
+    return math.tan(num1)
 
 #Function to take one input 
 def one_input():
-    num = int(input(:Enter one number: "))
+    num = int(input("Enter first number: "))
 	return num
 
 #Function to take two input 
 def two_input():
-    num_1 = int(input(:Enter one number: ")) 
+    num_1 = int(input("Enter first number: ")) 
     num_2 = int(input("Enter second number: "))
 	return num_1,num_2
 
@@ -43,41 +43,41 @@ print("Please select operation -\n"\
         "1. Add\n"\
         "2. Subtract\n"\
         "3. Multiply\n"\
-        "4. Divide\n")
-
-    number_1 = 0
-    number_2 - 0
+        "4. Divide\n"\
+        "5. Sin\n"\
+        "6. Cos\n"\
+        "7. Tan\n")
 
 # Take input from the user  
-select = int(input("Select operation among 1, 2, 3, 4: ")) 
+select = int(input("Select operation among 1, 2, 3, 4, 5, 6, 7: ")) 
   
 if select == 1: 
     number_1,number_2=two_input()
     print("\n", number_1, "+", number_2, "=", add(number_1, number_2))
 
 elif select == 2: 
-	number_1,number_2=two_input()
+    number_1,number_2=two_input()
     print("\n", number_1, "-", number_2, "=", subtract(number_1, number_2)) 
 
 elif select == 3: 
-	number_1,number_2=two_input()
+    number_1,number_2=two_input()
     print("\n", number_1, "*", number_2, "=", multiply(number_1, number_2)) 
 
 elif select == 4: 
-	number_1,number_2=two_input()
+    number_1,number_2=two_input()
     print("\n", number_1, "/", number_2, "=", divide(number_1, number_2))
 
  elif select == 5:
-     number_1=one_input  ()
-     print("\nsin of ", number_1,"=",sinnum(number_1))
+    number_1=one_input  ()
+    print("\nsin of ", number_1,"=",sinnum(number_1))
 
  elif select == 6:
-     number_1=one_input()  
-     print("\ncos of ", number_1,"=",cosnum(number_1))
+    number_1=one_input()  
+    print("\ncos of ", number_1,"=",cosnum(number_1))
 
  elif select == 7:
-     number_1=one_input()  
-     print("\ntan of ", number_1,"=",tannum(number_1))
+    number_1=one_input()  
+    print("\ntan of ", number_1,"=",tannum(number_1))
 
 else: 
     print("\nInvalid input")
