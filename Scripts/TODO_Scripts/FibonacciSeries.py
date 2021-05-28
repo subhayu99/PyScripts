30bf16ac9d602bdac4b681a9f9dbf3798dab0190
Prathima Kadari <kadariprathima4@gmail.com>
2021-05-28 18:49:24 +0530

    Added FibonacciSeries program

---
+++ Scripts/TODO_Scripts/FibonacciSeries.py
@@ -0,0 +1,13 @@
+#Program to find the factorial of given number
+
+
+def factorial(n):
+    i=1
+    fact = 1
+    while i < n+1:
+        fact = fact*i
+        i= i+1
+    print(fact)
+
+n = int(input('enter no to find factorial : '))
+factorial(n)
